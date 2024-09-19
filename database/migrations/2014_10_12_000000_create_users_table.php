3e4da81268ded3b8a8a719510cd0522639e8f904
doquangquyet <queytdo01698705281@gmail.com>
2024-09-20 00:29:21 +0700

    f

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar_img')->nullable();
-            $table->string('phone')->unique()->nullable(false);
+            $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->enum('role', ['user', 'staff', 'admin'])->default('user');
             $table->rememberToken();
