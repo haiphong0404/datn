2960fe410b0e7d48593bc58ccdf87394ed84a9a5
dovanduc03 <ducdovan203@gmail.com>
2024-11-08 15:52:44 +0700

    update_ui_login_logout

--- frontend/Client/src/components/Header.jsx
+++ frontend/Client/src/components/Header.jsx
@@ -33,9 +33,9 @@ const Header = () => {
     return localCart.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
-  const { userInfo } = useLoginForm();
+  const { userInfo , handleLogout  } = useLoginForm();
   console.log("Thông tin người dùng trong Account_info:", userInfo);
-  
+
   const products = useSelector((state) => state.product?.products || []);
   console.log(products);
   return (
@@ -67,7 +67,7 @@ const Header = () => {
                 <ul className="user-info-block">
                   <li>
                     <Link to="/my_account">
-                      <i className="fa fa-user-circle" /> {userInfo?.username || 'Tài khoản'}
+                      {JSON.parse(localStorage.getItem("userInfo"))?.username || "Tài khoản"}
                     </Link>
                   </li>
                   <li>
@@ -76,9 +76,17 @@ const Header = () => {
                     </Link>
                   </li>
                   <li>
-                    <a href="/register">
-                      <i className="fa fa-sign-in" /> Đăng nhập
-                    </a>
+                  {JSON.parse(localStorage.getItem("userInfo")) ? (
+                      // Nếu đã đăng nhập, hiển thị Đăng xuất
+                      <a href="#" onClick={handleLogout}>
+                        <i className="fa fa-sign-out" /> Đăng xuất
+                      </a>
+                    ) : (
+                      // Nếu chưa đăng nhập, hiển thị Đăng nhập
+                      <a href="/login">
+                        <i className="fa fa-sign-in" /> Đăng nhập
+                      </a>
+                    )}
                   </li>
                 </ul>
               </div>
--- frontend/Client/src/hooks/useLoginForm.js
+++ frontend/Client/src/hooks/useLoginForm.js
@@ -26,6 +26,7 @@ export const useLoginForm = (isDisplay) => {
   const [success, setSuccess] = useState("");
   const [userInfo, setUserInfo] = useState(null);
   const navigate = useNavigate();
+  
 
   // Lấy thông tin người dùng từ localStorage nếu có
   useEffect(() => {
@@ -80,10 +81,16 @@ export const useLoginForm = (isDisplay) => {
   }
 
   const handleLogout = () => {
+    // Xóa thông tin người dùng và token khỏi state và localStorage
     setUserInfo(null);
     localStorage.removeItem("userInfo");
+    localStorage.removeItem("token");
+  
+    // Chuyển hướng đến trang đăng nhập và bắt buộc tải lại trang
+    
     navigate("/login");
   };
+  
 
 
   const updateUserInfo = async (id, updatedInfo) => {
