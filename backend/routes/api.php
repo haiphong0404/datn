148047d592574678b8b726e3c0454be9db9834ae
dovanduc03 <ducdovan203@gmail.com>
2024-11-15 21:40:21 +0700

    push

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use App\Http\Controllers\Api\ArticlesController;
@@ -101,6 +100,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/cart/update', [CartController::class, 'updateCart']);
     Route::delete('/cart/remove', [CartController::class, 'removeFromCart']);
     Route::delete('/cart/remove/{product_variant_id}', [CartController::class, 'removeFromCart']);
+    Route::apiResource('order', OrderController::class );
 });
 Route::get('products/{productId}/variants', [ProductVariantController::class, 'index']);
 Route::get('/variants/{id}', [ProductVariantController::class, 'show']);
@@ -113,5 +113,5 @@ Route::get('contacts', [ContactController::class, 'index']); // Lấy danh sách
 Route::get('contacts/{id}', [ContactController::class, 'show']); // Lấy contact theo ID
 Route::get('/banners', [BannerController::class, 'index']);
 
-Route::apiResource('order', OrderController::class );
+
 Route::get('orders', [OrderController::class, 'abc']);
