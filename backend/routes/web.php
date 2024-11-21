74813fed5ec4486ecac0af2591300955ba0f12fb
twirke <126968260+twirke@users.noreply.github.com>
2024-11-21 21:07:46 +0700

    thống kê dashboard admin

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Admins\ProfileControllers;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admins\VoucherController;
+use App\Http\Controllers\StatisticsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +30,7 @@ Route::get('/', function () {
 
 // Route profile
 Route::middleware('auth')->group(function () {
-    Route::get('/admin', [AdminTestController::class, 'index'])->name('admin.index');
+    Route::get('/admin', [StatisticsController::class, 'index'])->name('admin.index');
     Route::get('profile', [ProfileControllers::class, 'index'])->name('admin.profile');
     Route::get('/profile/edit', [ProfileControllers::class, 'edit'])->name('admin.profile.edit');
     Route::put('/profile', [ProfileControllers::class, 'update'])->name('admin.profile.update');
